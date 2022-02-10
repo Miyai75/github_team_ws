4a7f5e292dcdf8b413ebafd75b818e2a46aab901
zyoushisu-ayaka <saika-maycat0308@dune.ocn.ne.jp>
2022-02-10 20:22:34 +0900

    引き算追加したよん

--- src/index.js
+++ src/index.js
@@ -128,7 +128,10 @@
 	/* 演習の編集範囲 はじめ */
 
 	/* 演習1 ここから */
-	// TODO: このコメントアウトを削除して書く
+	//引き算用の関数 sub
+	function sub(){
+		eqlNum.value = parseFloat(midNum[0].value) - parseFloat(midNum[1].value);
+	}
 	/* 演習1 ここまで */
 
 	/* TODO: 演習2 以降は下記に自由に記入 */
